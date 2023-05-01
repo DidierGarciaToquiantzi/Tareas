ab3bb83e6bd0820a8e885ca3766f3b2735f4322c
DidierGarciaToquiantzi <124199143+DidierGarciaToquiantzi@users.noreply.github.com>
2023-05-01 13:10:16 -0600

    Add files via upload

---
+++ tarea 8/java/ejemplo18.js
@@ -0,0 +1,17 @@
+function Obtener() {
+    fetch("https://yesno.wtf/api")
+    .then(function(response) {
+      return response.json();
+    })
+    
+    .then(function(json){
+      console.log(json);
+      let imagen = document.getElementById("imagen");
+      imagen.src = json.image;
+      let texto = documentgetElementById('contenedorTexto');
+      texto.innerHTML = json.answer;
+    });
+  }
+  
+  let boton = document.getElementById('boton');
+  boton.addEventListener('click', Obtener);
---
+++ tarea 8/java/ejemplo9.js
@@ -0,0 +1,10 @@
+function onKeyUp(event) {
+    console.log('onKeyUp:' + event.key);
+    }
+    document.addEventListener('keyup', onKeyUp);
+    
+var article=document.getElementById('electriccars');
+
+article.dataset.columns //"3"
+article.dataset.indexNumber //"12314"
+article.dataset.parent//"cars"
